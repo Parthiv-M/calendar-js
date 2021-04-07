cdc105263d4dc9521e722254d9b43062a5941a0f
PalChheda <chhedapal@yahoo.in>
2021-04-07 10:43:19 +0530

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,21 +1,21 @@
-var dt = new Date();
+let dt = new Date();
 document.getElementById("date-str").innerHTML = dt.toDateString();
 
 function renderDate() {
     dt.setDate(1);
 
-    var endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
+    const endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate();
 
-    var prev = new Date(dt.getFullYear(), dt.getMonth(), 0);
-    var prevDate = prev.getDate();
+    const prev = new Date(dt.getFullYear(), dt.getMonth(), 0);
+    const prevDate = prev.getDate();
 
-    var today = new Date();
+    const today = new Date();
 
-    var monthslist = ["January", "February", "March", "April", 'May', "June", "July", "August", "September", "October", "November", "December"];
+    const monthslist = ["January", "February", "March", "April", 'May', "June", "July", "August", "September", "October", "November", "December"];
 
     document.getElementById("month").innerHTML = monthslist[dt.getMonth()] + " " + dt.getFullYear();
 
-    var cell = "";
+    let cell = "";
 
     for (x = prev.getDay(); x >= 0; x--) {
         cell += "<div class = 'prev_date' >" + (prevDate - x) + "</div>";
@@ -30,7 +30,7 @@ function renderDate() {
 
     document.getElementsByClassName("date")[0].innerHTML = cell;
 
-    var divs = document.getElementsByClassName("current_month");
+    let divs = document.getElementsByClassName("current_month");
 
     for (i = 0; i < divs.length; i++)
         divs[i].setAttribute("id", i + 1);
@@ -42,8 +42,8 @@ function renderDate() {
 
 //function to change the date in date-str to the one clicked by the user 
 function display() {
-    var x = this.innerHTML;
-    var p = new Date(dt.getFullYear(), dt.getMonth(), x);
+    const x = this.innerHTML;
+    const p = new Date(dt.getFullYear(), dt.getMonth(), x);
     document.getElementById("date-str").innerHTML = p.toDateString();
 }
 
@@ -54,7 +54,7 @@ function moveDate(para) {
     } else if (para == 'next') {
         dt.setMonth(dt.getMonth() + (1));
     }
-    var A= new Date();
+    const A= new Date();
     document.getElementsByClassName("date-str").innerHTML = A.toDateString();
     renderDate();
 }
